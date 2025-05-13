7d8bcb79ac8cfd926f8deccb13a4072d9efba448
BT_4N2_03 <sample@bths.edu>
2025-05-13 13:57:24 -0400

    First

---
+++ main.py
@@ -0,0 +1,35 @@
+import mysql.connector
+
+def connectToDatabase():
+    connection = mysql.connector.connect(user='emilm24',
+                                         password='247075393',
+                                         host='10.8.37.226',
+                                         database='emilm24_db')
+    return connection
+
+def execute(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results=[]
+    for row in cursor:
+        results.append(row)
+    cursor.close()
+    connection.close()
+    return results
+
+def get_student_schedule(student_id):
+    query = f"CALL GetSchedule('{student_id}')"
+    return execute(connectToDatabase(), query)
+
+student_id=input("Enter a student id: ")
+results=get_student_schedule(student_id)
+for result in results:
+    class_name=str(result[0])
+    room=str(result[1])
+    period=str(result[2])
+    teacher=str(result[3])
+    print("Period: " + period)
+    print("Course:" + class_name)
+    print("Room:" + room)
+    print("Teacher:" + teacher)
+    print()
